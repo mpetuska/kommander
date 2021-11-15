bb4c9d80aa0dec3f8101aeaac64d037dc95499ed
Martynas Petuška <martynas@petuska.dev>
2021-11-15 19:15:51 +0000

    Retention

--- lib/kommander-core/src/commonMain/kotlin/util/annotations.kt
+++ lib/kommander-core/src/commonMain/kotlin/util/annotations.kt
@@ -1,13 +1,17 @@
 package dev.petuska.kommander.core.util
 
 @DslMarker
+@Retention(AnnotationRetention.SOURCE)
 public annotation class KommanderOptionDsl
 
 @DslMarker
+@Retention(AnnotationRetention.SOURCE)
 public annotation class KommanderCommandDsl
 
 @DslMarker
+@Retention(AnnotationRetention.SOURCE)
 public annotation class KommanderArgumentDsl
 
 @DslMarker
+@Retention(AnnotationRetention.SOURCE)
 public annotation class KommanderProgramDsl
